ad9cc0af0b0cb13fbab46141b887ea8fe397664b
Turntaker <turnatker@gmail.com>
2016-06-27 23:21:13 +1000

    Implemented KeyListener. Paddle can move using arrows.

--- Pong/src/main/Paddle.java
+++ Pong/src/main/Paddle.java
@@ -3,12 +3,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Paddle extends JPanel 
+public class Paddle extends JPanel implements KeyListener
 {
 	private final int yPos = 550;
 	//xPos will change. Haven't implemented mouse listener.
 	private int xPos = 30;
 	
+	public Paddle()
+	{
+		//adds keylistener to Paddle
+		addKeyListener(this);
+		setFocusable(true);
+	}
 	//overriding the super class method to draw rectangle. 
 	@Override 
 	public void paintComponent(Graphics g) 
@@ -19,5 +25,30 @@ public class Paddle extends JPanel
 		g.fillRect(xPos, yPos, 100, 20);
 		
 		
+	}
+	
+	public void keyTyped(KeyEvent e) 
+	{
+		//not used
+	}
+	
+	public void keyPressed(KeyEvent e)
+	{
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_RIGHT)
+		{
+			repaint();
+			xPos = xPos + 20;
+		}
+		else if (code == KeyEvent.VK_LEFT) 
+		{
+			repaint();
+			xPos = xPos - 20;
+		}
+	}
+	
+	public void keyReleased(KeyEvent e)
+	{
+		//not used
 	}
 }
