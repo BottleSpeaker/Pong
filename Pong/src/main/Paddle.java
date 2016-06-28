c44457180833df6564202299d71677adecec5af1
Turntaker <turnatker@gmail.com>
2016-06-28 13:24:09 +1000

    added super advanced collision against border

--- Pong/src/main/Paddle.java
+++ Pong/src/main/Paddle.java
@@ -1,55 +1,73 @@
 package main;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Paddle extends JPanel implements KeyListener
 {
+	// yPos is final.
 	private final int yPos = 550;
-	//xPos changes depending on key press. 
+	// xPos changes depending on key press.
 	private int xPos = 330;
-	
+
 	public Paddle()
 	{
-		//adds keylistener to Paddle
+		// adds keylistener to Paddle
 		addKeyListener(this);
-		//sets JPanel Focusable
+		// sets JPanel Focusable
 		setFocusable(true);
+
 	}
-	//overriding the super class method to draw rectangle. 
-	@Override 
-	public void paintComponent(Graphics g) 
+
+	// overriding the super class method to draw rectangle.
+	@Override
+	public void paintComponent(Graphics g)
 	{
-		super.paintComponent(g); 
+		super.paintComponent(g);
 		setBackground(Color.BLACK);
 		g.setColor(Color.WHITE);
 		g.fillRect(xPos, yPos, 100, 20);
-		
-		
+
 	}
-	
-	public void keyTyped(KeyEvent e) 
+
+	public void keyTyped(KeyEvent e)
 	{
-		//not used
+		// not used
 	}
-	
+
 	public void keyPressed(KeyEvent e)
 	{
+
 		int code = e.getKeyCode();
+
 		if (code == KeyEvent.VK_RIGHT)
 		{
 			repaint();
-			xPos = xPos + 20;
-		}
-		else if (code == KeyEvent.VK_LEFT) 
+			xPos = xPos + 11;
+		} else if (code == KeyEvent.VK_LEFT)
 		{
 			repaint();
-			xPos = xPos - 20;
+			xPos = xPos - 11;
 		}
+		
+//SUPER ADVANCED COLLISION CHECKING
+		if (xPos > 720 && xPos < 740)
+		{
+			repaint();
+			xPos = xPos - 21;
+		}
+		else if (xPos < -20)
+		{
+			repaint();
+			xPos = xPos + 21;
+		}
+
 	}
-	
+
 	public void keyReleased(KeyEvent e)
 	{
-		//not used
+		// not used
 	}
+
 }
